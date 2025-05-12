afe8a0b2331357fd5064052262567912ec4a32a2
cdryampi <cdryampi@gmail.com>
2025-05-12 10:55:04 +0200

    Refactor settings.py to use environment variables for SECRET_KEY and DEBUG; change LANGUAGE_CODE to Spanish

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__(nt)ll37ey@%464si#+6er8v_@zr$-_1czu1s$#hjehyr8r#'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-!@#4$%^&*()_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
